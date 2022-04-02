59fb8f6047f979d0d6b9d0c7025b776d26f6ba78
haidi <haidinurhadinata@gmail.com>
2022-04-02 09:59:17 +0800

    update env and gitignore

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,8 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Initialise environment variables
 env = environ.Env()
-# reading .env file
-environ.Env.read_env()
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
