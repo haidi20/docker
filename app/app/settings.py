457f3efdc615477fc7c5585658a5e63a6784866d
haidi <haidinurhadinata@gmail.com>
2022-04-02 11:35:01 +0800

    update settings.py bagian secret_key

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY", env("SECRET_KEY", default="unsafe-secret-key"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", env("DEBUG"))
