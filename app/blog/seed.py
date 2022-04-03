8f1b74583d37005fa5ab10c1c1cda8c11a736ee3
haidi <haidinurhadinata@gmail.com>
2022-04-03 11:46:29 +0800

    only jenkins

--- app/blog/seed.py
+++
@@ -1,9 +0,0 @@
-from django_seed import Seed
-
-seeder = Seed.seeder()
-
-from .models import Category, Post
-seeder.add_entity(Category, 5)
-seeder.add_entity(Post, 10)
-
-inserted_pks = seeder.execute()
